c95d3edff9d7edc34dc14faa28a6984dcaa017b9
github kanato-hyakutome <kanato-hyakutome@kccs.co.jp>
2025-07-24 12:57:12 +0900

    フォームクラスの訂正

--- src/main/java/com/example/attendance/form/AccountRegistForm.java
+++ src/main/java/com/example/attendance/form/AccountRegistForm.java
@@ -9,11 +9,11 @@ import lombok.Data;
 public class AccountRegistForm {
 
     @NotBlank(message = "社員IDは必須です。")
-    @Size(min = 1, max = 10, message = "社員IDは1文字から10文字で指定してください。")
+    @Size(min = 6, max = 6, message = "社員IDは6文字で指定してください。")
     private String employeeId;
 
     @NotBlank(message = "氏名は必須です。")
-    @Size(min = 1, max = 50, message = "氏名は1文字から50文字で指定してください。")
+    @Size(min = 1, max = 20, message = "氏名は1文字から20文字で指定してください。")
     private String fullName;
 
     @NotBlank(message = "部署を選択してください。")
@@ -21,6 +21,14 @@ public class AccountRegistForm {
 
     @NotBlank(message = "役職を選択してください。")
     private String position;
+
+    @NotBlank(message = "パスワードは必須です。")
+    @Size(min = 10, max = 30, message = "パスワードは10文字から30文字で指定してください。")
+    private String password;
+
+    // 確認用パスワードフィールドを追加する場合
+    // @NotBlank(message = "確認用パスワードは必須です。")
+    // private String confirmPassword;
 
     // 部署と役職のオプションリストを保持するためのフィールドを追加することもできます
     // これらはコントローラーで設定し、ビューで使用することができます
