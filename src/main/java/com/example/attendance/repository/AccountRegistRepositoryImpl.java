88786c51ec60bf7538d2f3cc66e198b014833026
github Rion-Ishimaru <rion-ishimaru@kccs.co.jp>
2025-07-24 15:34:10 +0900

    Repositoryの作成一旦終了

--- src/main/java/com/example/attendance/repository/AccountRegistRepositoryImpl.java
+++ src/main/java/com/example/attendance/repository/AccountRegistRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.example.attendance.repository;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.example.attendance.entity.Account;
@@ -10,31 +11,19 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AccountRegistRepositoryImpl implements AccountRegistRepository {
 
+private final JdbcTemplate jdbcTemplate;
+	
 	@Override
 	public void add(Account acount) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		String sql = "INSERT INTO application"
+			+ "(employee_id, department_id, job_id, name)"
+			+ "VALUES (?, ?, ?, ?)";
+		
+			jdbcTemplate.update(sql,
+			acount.getEmployeeId(),
+			acount.getDepartmentId(),
+			acount.getJobId(),
+			acount.getName()
+			);
 	}
-
 }
-
-
-
-
-//import org.springframework.jdbc.core.JdbcTemplate;
-
-//public class ApplicationRepositoryImpl implements ApplicationRepository {
-//	private final JdbcTemplate jdbcTemplate;
-//	@Override
-//	public void add(Application application) {
-//		String sql = "INSERT INTO application"
-//				+ "(employee_id, category_id, comment, date_apply)"
-//				+ "VALUES (?, ?, ?, ?)";
-//		jdbcTemplate.update(sql,
-//				application.getEmployeeId(),
-//				application.getCategoryId(),
-//				application.getComment(),
-//				application.getDateApply()
-//				);
-//	}
-//}
