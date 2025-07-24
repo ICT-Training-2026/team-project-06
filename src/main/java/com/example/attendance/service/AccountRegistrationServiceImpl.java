ccb89687b5be4b2cdcdbb9eafd5d0003115f7b1d
github kanato-hyakutome <kanato-hyakutome@kccs.co.jp>
2025-07-24 14:14:45 +0900

    AccountRegistrationServiceImplの作成

---
+++ src/main/java/com/example/attendance/service/AccountRegistrationServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.attendance.service;
+
+import com.example.attendance.entity.Account;
+
+public class AccountRegistrationServiceImpl implements AccountRegistrationService {
+
+	@Override
+	public void register(Account account) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
