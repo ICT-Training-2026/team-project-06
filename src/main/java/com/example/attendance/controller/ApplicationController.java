34a0be85606282db37aa189d698bb86755bd6e4e
github Naoki-Yashiroda <Naoki-Yashiroda@kccs.co.jp>
2025-07-24 13:13:37 +0900

    稼働確認
    htmlファイル src/main/resources/templates/に配置すること
    cssファイル src/main/resources/static/css/に配置すること
    application.propaties tymeleafの記述の追加 dbの名前指定
    TeamProject06Application.java 新規作成 これがないとSpring起動できない
    pom テスト用下にtymeleafと起動用設定の追記

--- src/main/java/com/example/attendance/controller/ApplicationController.java
+++ src/main/java/com/example/attendance/controller/ApplicationController.java
@@ -3,6 +3,7 @@ package com.example.attendance.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -19,14 +20,20 @@ public class ApplicationController {
 
 	private final ApplicationService service;
 
-	//申請の可否はjavascript側で判断してもらうか, Postの値は応相談
-	@PostMapping("/confirm-application")
+	@GetMapping("/application")
+	private String request() {
+		return "request";
+	}
+
+
+	//申請の可否はjavascript側で判断してもらうか→攻撃の可能性
+	@PostMapping("/api/submit-request")
 	private String application(
 			@Validated @ModelAttribute ApplicationForm form,
 			BindingResult result) {
 
 		if (result.hasErrors()) {
-			return "application";
+			return "request";
 		}
 
 		Application a = new Application();
@@ -36,6 +43,6 @@ public class ApplicationController {
 		a.setDateApply(null);
 		service.regist(a);
 
-		return "application";
+		return "request";
 	}
 }
