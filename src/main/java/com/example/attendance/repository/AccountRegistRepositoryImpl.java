4a627266d393071ac1f7190e1c9717fbb12ea53c
github Rion-Ishimaru <rion-ishimaru@kccs.co.jp>
2025-07-24 14:50:31 +0900

    Implの作成途中経過

--- src/main/java/com/example/attendance/repository/AccountRegistRepositoryImpl.java
+++ src/main/java/com/example/attendance/repository/AccountRegistRepositoryImpl.java
@@ -1,7 +1,13 @@
 package com.example.attendance.repository;
 
+import org.springframework.stereotype.Repository;
+
 import com.example.attendance.entity.Account;
 
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
 public class AccountRegistRepositoryImpl implements AccountRegistRepository {
 
 	@Override
@@ -11,3 +17,24 @@ public class AccountRegistRepositoryImpl implements AccountRegistRepository {
 	}
 
 }
+
+
+
+
+//import org.springframework.jdbc.core.JdbcTemplate;
+
+//public class ApplicationRepositoryImpl implements ApplicationRepository {
+//	private final JdbcTemplate jdbcTemplate;
+//	@Override
+//	public void add(Application application) {
+//		String sql = "INSERT INTO application"
+//				+ "(employee_id, category_id, comment, date_apply)"
+//				+ "VALUES (?, ?, ?, ?)";
+//		jdbcTemplate.update(sql,
+//				application.getEmployeeId(),
+//				application.getCategoryId(),
+//				application.getComment(),
+//				application.getDateApply()
+//				);
+//	}
+//}
