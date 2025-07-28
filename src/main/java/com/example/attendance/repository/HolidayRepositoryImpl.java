1969e204b83bd29d2251b1b239972685b8ba7512
github Rion-Ishimaru <rion-ishimaru@kccs.co.jp>
2025-07-28 12:51:28 +0900

    リポジトリー作成

--- src/main/java/com/example/attendance/repository/HolidayRepositoryImpl.java
+++ src/main/java/com/example/attendance/repository/HolidayRepositoryImpl.java
@@ -1,5 +1,29 @@
 package com.example.attendance.repository;
 
-public class HolidayRepositoryImpl {
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
+import lombok.RequiredArgsConstructor;
+
+@Repository
+@RequiredArgsConstructor
+public class HolidayRepositoryImpl implements HolidayRepository {
+	
+private final JdbcTemplate jdbcTemplate;
+
+@Override
+public void add(Holiday holiday) {
+	String sql = "INSERT INTO holiday"
+        + "(employee_id, total_work, total_overtime, take_vacation, month)"
+        + "VALUES (?, ?, ?, ?, ?)";
+	
+	
+		jdbcTemplate.update(sql,
+            holiday.getEmployeeId(),
+            holiday.getTotalWork(),
+            holiday.getTotalOvertime(),
+            holiday.getTakeVacation(),
+            holiday.getMonth()
+		);
+	}
 }
