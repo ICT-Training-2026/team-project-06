2c43a82ea05a4db6de41861e32f4874cca7117b3
github Naoki-Yashiroda <Naoki-Yashiroda@kccs.co.jp>
2025-07-31 16:51:41 +0900

    AWS用にプロジェクトを更新

--- src/main/java/com/example/attendance/TeamProject06Application.java
+++ src/main/java/com/example/attendance/TeamProject06Application.java
@@ -3,12 +3,20 @@ package com.example.attendance;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan("com.example.attendance.entity")
-public class TeamProject06Application {
+public class TeamProject06Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TeamProject06Application.class, args);
-    }
+	@Override
+	protected SpringApplicationBuilder
+	configure(SpringApplicationBuilder application) {
+		return application.sources(TeamProject06Application.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProject06Application.class, args);
+	}
 }
